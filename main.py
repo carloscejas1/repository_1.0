e4f00b11e7226962f46b91470f58dd79aebe9f79
carlos cejas <carloscejas1@icloud.com>
2026-04-06 00:04:43 -0300

    Revert "Add explanatory print about f-string"
    This reverts commit e9c39fd7aaabf65eda51679f667770507c2f628f.

--- main.py
+++ main.py
@@ -3,4 +3,3 @@ print("Hola Crack!")
 nombre = input("Como te llamas bestia pop? ")
 
 print(f"Vas muy bien {nombre}, no le aflojes, todo va a mejorar")
-print("Se esquivoco a proposito con el f-string hace rato, segun el")
